1b5ce583f080f50569daae400c1e7325597efb46
Pavel Repin <76727704+IPRepin@users.noreply.github.com>
2024-09-06 10:53:50 +0300

    add Gigachain

--- giga_chat/generators_gigachat.py
+++ giga_chat/generators_gigachat.py
@@ -1,52 +1,45 @@
-import re
-from gigachat import GigaChat
-from gigachat.models import Chat, Messages, MessagesRole, Image
-# from aiofile import async_open
+"""Пример работы с чатом через gigachain"""
+import logging
+import asyncio
+from langchain_community.chat_models.gigachat import GigaChat
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+
 from config import settings
+
+logger = logging.getLogger(__name__)
+
+async def get_connection():
+    llm = GigaChat(credentials=settings.GIGACHAT_AUTHORIZATION,
+                    verify_ssl_certs=False)
+    chat = ConversationChain(
+        llm=llm,
+        memory=ConversationBufferMemory(),
+    )
+
+    return chat
 
 
 async def get_gigachat_text(bot_message: str) -> str:
-    payload = Chat(
-        model="",
-        messages=[
-            Messages(
-                role=MessagesRole.SYSTEM,
-                content="Ты опытный интернет маркетолог разбираешься во множестве"
-                        "инструментов таких как: яндекс директ, яндекс метрика,"
-                        "VK target, MyTarget. Можешь грамотно написать продающие тексты для "
-                        "объявлений. Способен написать оптимальные SEO тексты. "
-                        "Твоя задача помогать другим маркетологам с настройкой рекламных компаний "
-                        "анализом рынка, написанием текстов для объявлений, и статей,"
-                        " максимально подробно отвечать на вопросы, подкреплять свои ответы "
-                        "ссылками на материалы, документацию и видео по теме вопроса."
-            )
-        ],
-        temperature=1.5,
-        max_tokens=4096,
-
-    )
-    async with GigaChat(credentials=settings.GIGACHAT_AUTHORIZATION, verify_ssl_certs=False) as giga:
-        payload.messages.append(Messages(role=MessagesRole.USER, content=bot_message))
-        response = giga.chat(payload)
-        return response.choices[0].message.content
-
-
-async def get_gigachat_image(msg: str) -> str:
-    payload = Chat(
-        messages=[Messages(role=MessagesRole.USER, content=msg)],
-        temperature=0.7,
-        max_tokens=4096,
-        function_call="auto",
-    )
-    async with GigaChat(credentials=settings.GIGACHAT_AUTHORIZATION, verify_ssl_certs=False) as giga:
-        response = giga.chat(payload)
-        text = response.choices[0].message.content
-        match = re.search(r'src="([^"]+)"', text)
-        image: Image = await giga.aget_image(file_id=match.group(1))
-        return image
-
-
-async def get_quantity_tokens() -> int:
-    async with GigaChat(credentials=settings.GIGACHAT_AUTHORIZATION, verify_ssl_certs=False) as giga:
-        result = giga.tokens_count(input_=["12345"])
-        return result
+    messages = [
+        SystemMessage(
+            content="Ты опытный интернет маркетолог разбираешься во множестве"
+                    "инструментов таких как: яндекс директ, яндекс метрика,"
+                    "VK target, MyTarget. Можешь грамотно написать продающие тексты для "
+                    "объявлений. Способен написать оптимальные SEO тексты. "
+                    "Твоя задача помогать другим маркетологам с настройкой рекламных компаний "
+                    "анализом рынка, написанием текстов для объявлений, и статей,"
+                    " максимально подробно отвечать на вопросы, подкреплять свои ответы "
+                    "ссылками на материалы, документацию и видео по теме вопроса."
+                    "На все вопросы давай полные, развернутые ответы."
+        )
+    ]
+    chat = await get_connection()
+    while True:
+        if bot_message == "💬Новый запрос":
+            break
+        messages.append(HumanMessage(content=bot_message))
+        chat.invoke(messages)
+        bot_answer = chat.memory.chat_memory.messages[-1].content
+        return bot_answer
--- handlers/gigachat_hendlers.py
+++ handlers/gigachat_hendlers.py
@@ -3,7 +3,7 @@ import logging
 from aiogram import Router, types, F
 from aiogram.fsm.context import FSMContext
 
-from giga_chat.generators_gigachat import get_gigachat_text, get_quantity_tokens
+from giga_chat.generators_gigachat import get_gigachat_text
 from keyboards.gigachat_keyboards.gigachat_replay_keyboards import get_gigachat_keyboard
 from keyboards.replay_keyboards import get_main_keyboard
 from utils.states import ChatStates
@@ -14,6 +14,7 @@ logger = logging.getLogger(__name__)
 
 @gigachat_router.message(F.text.in_(["💬Новый запрос", "🤖AI маркетолог"]))
 async def get_chatting(message: types.Message, state: FSMContext) -> None:
+    await state.clear()
     await state.set_state(ChatStates.text)
     if message.text == "🤖AI маркетолог":
         await message.answer('Введите ваш запрос. Например "Что ты умеешь?"')
@@ -24,11 +25,8 @@ async def get_chatting(message: types.Message, state: FSMContext) -> None:
 @gigachat_router.message(ChatStates.text)
 async def get_chat_response(message: types.Message, state: FSMContext) -> None:
     gpt_answer = await get_gigachat_text(message.text)
-    await state.set_state(ChatStates.wait)
     await message.answer(gpt_answer,
                          reply_markup=await get_gigachat_keyboard())
-    await state.clear()
-    logger.error("Количество оставшихся токенов: %s", await get_quantity_tokens())
 
 
 @gigachat_router.message(ChatStates.wait)
